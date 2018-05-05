93feb4c362694993e76b80065c11831590094564
coppe-pan <pekocoppepan@gmail.com>
2018-05-05 21:36:40 +0900

    set configuration variables for video

--- base/video.js
+++ base/video.js
@@ -1,16 +1,27 @@
 module.exports = class Video {
-    describe() {
-        return {
-            'id': undefined,
-            'name': undefined,
-            'has': {
-                'searchVideos': true,
-                
-            }
-        }
-    }
-}
+  describe() {
+    return {
+      id: undefined,
+      name: undefined,
+      has: {
+        searchVideos: true,
+        getVideoById: true,
+        getVideoEmbedCode: true,
+        getDeletedVideo: true,
+        isVideoActive: true,
+        getCategoriesList: true,
+        getTagsList: true,
+        getStarList: false,
+        getStarDetailedList: false,
+        getAmateurList: false,
+        getAmateurDetailedList: false
+      },
+      urls: {
+        logo: undefined,
+        api: undefined,
+        www: undefined
+      }
+    };
+  }
+};
 
-const video = new Video ();
-
-console.log(video);
